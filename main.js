af784ce349bf63eb545578139c5fda41706aeeef
SharkAce <charles.marchildon2@gmail.com>
2022-03-28 09:22:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const length = 1000
-const height = 750
+const width = 1200
+const height = 800 
 const tileSize = 50
 
 new p5()
 
 function setup(){
-    let canvas = createCanvas(length, height)
+    let canvas = createCanvas(width, height)
     let pos = {
       x : (windowWidth - width) / 2,
       y : (windowHeight - height) / 2
@@ -14,12 +14,10 @@ function setup(){
 }
 
 const world = new World({
-    xLen: Math.floor(length/tileSize),
+    xLen: Math.floor(width/tileSize),
     yLen: Math.floor(height/tileSize),
-    bombQty: 4
+    bombQty: 90
 })
-
-world.createGrid(world.options)
 
 function draw(){
     background(245)
@@ -28,7 +26,7 @@ function draw(){
 
 
 //left click
-function mouseClicked(event){
+function mouseClicked(){
     if (!world.failed){
         world.click(
             Math.floor(mouseX/tileSize),
@@ -39,10 +37,15 @@ function mouseClicked(event){
 //right click   
 document.oncontextmenu = () => {
     if (!world.failed){
-        world.flag(
-            Math.floor(mouseX/tileSize),
-            Math.floor(mouseY/tileSize)
-        )
+        try {
+            world.flag(
+                Math.floor(mouseX/tileSize),
+                Math.floor(mouseY/tileSize)
+            )
+        }
+        catch(err){
+            return true
+        }
         return false
     }
 }
